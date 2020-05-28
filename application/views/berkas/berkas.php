6932f9f0b46fa07fdc2c5ed1ddbdcbc47c08b9a1
hendri2212 <cowok_cool320@yahoo.co.id>
2020-05-29 02:41:26 +0800

    berkas & pay

---
+++ application/views/berkas/berkas.php
@@ -0,0 +1,42 @@
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+	<h1 class="h2">Kelengkapan Berkas</h1>
+</div>
+<form action="" method="post">
+    <div class="form-group row">
+        <label class="col-lg-2">KTP</label>
+        <div class="col-lg-6">
+            <input type="text" name="" class="form-control">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label class="col-lg-2">Buku Nikah</label>
+        <div class="col-lg-6">
+            <input type="text" name="" class="form-control">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label class="col-lg-2">Akta Kelahiran</label>
+        <div class="col-lg-6">
+            <input type="text" name="" class="form-control">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label class="col-lg-2">Ijazah</label>
+        <div class="col-lg-6">
+            <input type="text" name="" class="form-control">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label class="col-lg-2">Passpor</label>
+        <div class="col-lg-6">
+            <input type="text" name="" class="form-control">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label class="col-lg-2">Kesehatan</label>
+        <div class="col-lg-6">
+            <input type="text" name="" class="form-control">
+        </div>
+    </div>
+    <input type="button" value="Simpan" class="btn btn-success">
+</form>
--- application/views/pembayaran/pembayaran.php
+++ application/views/pembayaran/pembayaran.php
@@ -1,5 +1,14 @@
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+	<h1 class="h2">Pembayaran</h1>
+	<div class="btn-toolbar mb-2 mb-md-0">
+		<div class="btn-group mr-2">
+			<input type="text" class="form-control border-secondary" placeholder="Type here">
+		</div>
+		<input type="button" value="Search" class="btn btn-info">
+	</div>
+</div>
 <div class="table-responsive">
-	<table class="table table-striped table-sm">
+	<table class="table table-striped table-sm table-hover">
 	  <thead>
 	    <tr>
 	      <th>#</th>
