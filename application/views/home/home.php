a7cf402dadbdd158ca0c6085a1b411e45a95031e
hendri2212 <cowok_cool320@yahoo.co.id>
2020-06-26 21:17:39 +0800

    update home

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -1,15 +1,13 @@
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
 	<h1 class="h2">Dashboard</h1>
-	<div class="btn-toolbar mb-2 mb-md-0">
+	<!-- <div class="btn-toolbar mb-2 mb-md-0">
 		<div class="btn-group mr-2">
 			<input type="text" class="form-control border-secondary" placeholder="Type here">
 		</div>
 		<input type="button" value="Search" class="btn btn-info">
-	</div>
+	</div> -->
 </div>
-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+<div class="text-center">
+	<h3 class="text-capitalize">selamat datang di pelayanan haji kotabaru</h3>
+	<img src="<?= base_url('./assets/img/logo.png') ?>" class="logos img-fluid my-3">
+</div>
