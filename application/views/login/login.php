25be99d1f221d209a93a1a5b310f0c37d29ae5cd
hendri2212 <cowok_cool320@yahoo.co.id>
2020-06-27 09:52:50 +0800

    update page login

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -10,7 +10,13 @@
 <div class="col-7 mx-auto mt-3">
     <img src="<?= base_url('./assets/img/logo.png') ?>" class="img-fluid w-25 mx-auto d-block">
     <h4 class="text-center text-uppercase mb-3">Kementerian Agama Kotabaru</h4>
-    <input type="text" name="" placeholder="Ketik ID Jama'ah" class="form-control form-control-lg rounded-0 border-bottom-0 border-secondary">
-    <input type="text" name="" placeholder="Ketik Password" class="form-control form-control-lg rounded-0 border-secondary">
-    <input type="button" value="Masuk" class="btn btn-block btn-success btn-lg rounded-0">
+	<form action="<?= base_url('./content/masuk') ?>" method="post">
+		<input type="text" name="username" placeholder="Ketik ID Jama'ah" class="form-control form-control-lg rounded-0 border-bottom-0 border-secondary">
+		<input type="text" name="password" placeholder="Ketik Password" class="form-control form-control-lg rounded-0 border-secondary">
+		<input type="submit" name="masuk" value="Masuk" class="btn btn-block btn-success btn-lg rounded-0">
+	</form>
+
+	<!-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#wrong">
+		Launch demo modal
+	</button> -->
 </div>
